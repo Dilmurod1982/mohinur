e0ef83379f44bf07a2e7b8a04ec7243984de77e1
cipher <dilsurbet@hmail.com>
2024-06-10 10:56:37 +0500

    1

--- js/request.js
+++ js/request.js
@@ -1,20 +1,19 @@
 import { loading } from "./loading.js";
+const overlay = document.getElementById("overlay");
 
 export const getData = async (url) => {
   try {
-    loading(true);
+    // overlay.classList.add("loading");
     const request = await fetch(url);
     if (!request.ok) {
       throw new Error("Nimadir noto'g'ri ketdi? Oma Uzr!");
     }
     const response = await request.json();
 
-    loading(false);
+    overlay.classList.remove("loading");
     return response;
   } catch (error) {
-    loading(false);
+    overlay.classList.remove("loading");
     return error.message;
   }
 };
-
-
